9eac176a7f8bff2843f69d4c714d56ea2c3ad6b6
Bashar <basharkhan71@gmail.com>
2018-12-23 12:48:43 +0600

    first commit

---
+++ src/main/java/EventManagement/model/SpecialEvent.java
@@ -0,0 +1,37 @@
+package EventManagement.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Document(collection = "specialEvent")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SpecialEvent {
+
+    private String id;
+    private String eventName;
+    private String eventAddress;
+    private String eventDescription;
+    private String url;
+    private String organizerName;
+    private String organizerDetails;
+    private String eventCreationDate;
+    private String eventDate;
+    private String buyEvent;
+    private String country;
+    private String date;
+    private String startTime;
+    private String endTime;
+    private int vacancy;
+    private String category;
+    private Set<SubEvent> subEvents = new HashSet<>();
+}
